c1f915493398d16006a77b43f70c8582e379e3c0
Roman khadka <162034685+Roman-khadka@users.noreply.github.com>
2024-05-30 11:18:51 +0000

    container config done

---
+++ 01_basics/01_Variable.js
@@ -0,0 +1,10 @@
+const accountId = "009";
+let accountEmail = "roman@gmail.com"
+var accountName = "Roman"
+accountCity = "ktm"
+
+//accId = 2 not allowed in JS
+accountEmail = "ram@gmail.com"
+accountName = "ram"
+accountCity = "BKT"
+console.table([accountId, accountEmail, accountName, accountCity]);
