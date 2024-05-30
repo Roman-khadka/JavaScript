63bf36b9bfb639941f856543a09239b96c37b5c2
Roman khadka <162034685+Roman-khadka@users.noreply.github.com>
2024-05-30 14:34:32 +0000

    container config done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict";// treats all JS code as newer code
+//alert(2+2) only works on browsers we are on node JS
+console.log(2
+    +2
+)// works but make code more readable
+console.log(2-1);//correct way to write a code
+
+/*
+String
+number => 2 to the power 53
+bigint 
+boolean => true/false
+null => stand alone value
+undefined => 
+    symbols => unique
+*/
+console.log(typeof 2);
