0dd8788402a229a3449fb96799c6d3745414b71b
Roman khadka <162034685+Roman-khadka@users.noreply.github.com>
2024-05-31 15:26:43 +0000

    comparision done

---
+++ 01_basics/04_Comparision.js
@@ -0,0 +1,10 @@
+//console.log(2=2);
+//console.log(2<1);
+//console.log((8-2)>4);
+//console.log(1=0);
+//console.log(0=0);
+
+//console.log("2">1)
+console.log(null < 0);
+console.log(null = 0);
+console.log(null <=0);
