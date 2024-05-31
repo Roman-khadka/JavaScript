629861ee02b424ff80bb7728215077da45bd0e19
Roman khadka <162034685+Roman-khadka@users.noreply.github.com>
2024-05-31 05:37:58 +0000

    Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,8 +1,8 @@
 let age = "1abc"
-console.log(typeof age);
+//console.log(typeof age);
 let valueinnumber = Number(age);
-console.log(typeof valueinnumber);
-console.log(valueinnumber);//=>converts string into number
+//console.log(typeof valueinnumber);
+//console.log(valueinnumber);//=>converts string into number
 
 /*
 "33" => 33
@@ -11,9 +11,32 @@ True => 1;False => 0
 */
 let isLoggedIn = 0;
 let BooleanisLoggedIn = Boolean(isLoggedIn);
-console.log(BooleanisLoggedIn)// converts 0 into false
+//console.log(BooleanisLoggedIn)// converts 0 into false
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber )//=> converts 33 into string
+//console.log(stringNumber)
+//console.log(typeof stringNumber )//=> converts 33 into string
+
+
+/*              Operation                                                */
+let value = 33
+let NegValue = -value
+//console.log(NegValue) 
+
+//console.log(2+2);
+//console.log(2-3);
+//console.log(2*3);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+let str1 = "hello"
+let str2 = " Roman"
+let str3 = str1 + str2
+//console.log(str3); 
+
+
+console.log(1 + 2);
+console.log(2 +"3");
+console.log("2"+ 3 + "2");
+console.log(2 + 2 +"2");
