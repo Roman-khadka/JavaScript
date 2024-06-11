998341e78b524030d607a9fec812a0c38aad62b2
Roman khadka <162034685+Roman-khadka@users.noreply.github.com>
2024-06-11 16:57:10 +0000

    function with object and array completed

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -19,13 +19,40 @@ function addNum(num1, num2){//parameter
    }
    
    const result = addNum(2, 2)
-   console.log("Result: ",result);
+  // console.log("Result: ",result);
 
    function Username(loginUsername){
     if(loginUsername === undefined){
-        console.log("Enter username");
+       // console.log("Enter username");
         return
      } 
      return `${loginUsername} just loggedd in`
     }
-   console.log(Username());
+   //console.log(Username());
+
+   function myCart(val1,val2,...num1){//rest ko use gareko
+    return num1
+   }
+   console.log(myCart(2000,1000,9200,3443));
+
+   const user = {
+    username:"Roman",
+    price:450
+   }
+
+   function handleObject(anyobject){
+       console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+   }
+   //handleObject(user)
+   handleObject({
+    username:"Sam",
+    price:230
+   })
+
+   const myarr = [200,399,400,500]
+
+   function secondHandler(newArray){
+    return newArray[0]
+   }
+
+   console.log(secondHandler(myarr))
